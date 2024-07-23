a1c45b9830d0ed70ce411f0e50016e409d79ac8a
Matthew Harvey <matthewtlharvey@gmail.com>
2024-07-23 03:48:18 +0100

    allow CORS for hello api route

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,11 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async () => {
-  return new Response("Hello, world!");
+
+  // set response headers to allow CORS
+  const headers = new Headers();
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+  return new Response("Hello, world!", { headers });
 };
