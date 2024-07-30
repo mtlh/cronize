f314156f1a6c70adae59bd844bd3dc140b612d8f
Matthew Harvey <matthewtlharvey@gmail.com>
2024-07-30 23:59:44 +0100

    awaits added for execution so status updated on time

--- src/pages/api/cronexecute.ts
+++ src/pages/api/cronexecute.ts
@@ -21,28 +21,36 @@ export const GET: APIRoute = async ({ request }) => {
         const abortController = new AbortController();
         const timeoutId = setTimeout(() => abortController.abort(), 8000); // 8000 ms = 8 seconds
         const requestHeaders = new Headers();
-        for (const [key, value] of JSON.parse(queue.rows[0].request_headers!.toString())) {
-            requestHeaders.append(key.toString(), value.toString());
-        }
-        fetch(`${queue.rows[0].url}`, { signal: abortController.signal, headers: requestHeaders, method: queue.rows[0].request_method!.toString() })
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return res;
-        })
-        .then(res => {
-            clearTimeout(timeoutId); // Clear the timeout if the request completes in time
-            runStatus = res.status;
-        })
-        .catch(error => {
-            if (error.name === 'AbortError') {
-                console.error('Fetch request timed out');
-            } else {
-                console.error('Fetch request failed', error);
-            }
+        // iterate through array json object ([{key: value}, {key: value}])
+        JSON.parse(queue.rows[0].request_headers!.toString()).forEach((item: {key: string, value: string}) => {
+            requestHeaders.append(item.key.toString(), item.value.toString());
         });
 
+        async function fetchData() {
+            try {
+                const res = await fetch(`${queue.rows[0].url}`, { 
+                    signal: abortController.signal, 
+                    headers: requestHeaders, 
+                    method: queue.rows[0].request_type!.toString() 
+                });
+                clearTimeout(timeoutId); // Clear the timeout if the request completes in time
+                runStatus = res.status;
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res;
+            } catch (error: any) {
+                if (error.name === 'AbortError') {
+                    console.error('Fetch request timed out');
+                } else {
+                    console.error('Fetch request failed', error);
+                }
+                throw error;
+            }
+        }
+        await fetchData()
+
+        console.log(runStatus)
         const execute = await connectdb().execute({
             sql: `INSERT INTO CronjobHistory (cronjob_id, status, ran_time) VALUES (?, ?, CURRENT_TIMESTAMP);`,
             args: [queue.rows[0].cronjob_id, runStatus.toString()]
